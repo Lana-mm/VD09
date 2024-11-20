62f8d4bb820ff9105a0c9c4c6882bee6031426a4
Lana-mm <malimonovas@list.ru>
2024-11-20 14:36:30 +0500

    end2

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
-from Flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash
 from app import app, db, bcrypt
 from app.models import User
 from app.forms import LoginForm, RegistrationForm
-from flask_login import login, logout, current_user, login_required
+from flask_login import login_user, logout_user, current_user, login_required
 
 @app.route('/')
 @login_required
@@ -11,8 +11,8 @@ def index():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if current_user.is.authenticated:
-        return redirect(url_for('index.html'))
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -22,24 +22,25 @@ def register():
         flash('Вы успешно зарегистрировались!', 'success')
         return redirect(url_for('login'))
     return render_template("register.html", form=form)
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index.html'))
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login(user)
+            login_user(user)
             return redirect(url_for('index'))
         else:
             flash('Неверно введены данные аккаунта', 'danger')
     return render_template("login.html", form=form)
-@app.route('/logout', methods=['GET', 'POST'])
+
+@app.route('/logout')
 def logout():
-    logout()
+    logout_user()
     return redirect(url_for('login'))
+
 @app.route('/click')
 @login_required
 def click():
